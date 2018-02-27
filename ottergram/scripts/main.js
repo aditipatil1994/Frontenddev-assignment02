fcc73911df66f67615b6ed701babb1f5319f3d2b
aditipatil1994 <aditipatil1994@gmail.com>
2018-02-26 18:02:29 -0800

    Eslint error fixed.

--- ottergram/scripts/main.js
+++ ottergram/scripts/main.js
@@ -1,38 +1,38 @@
-var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
-var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
-var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
-var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
-var HIDDEN_DETAIL_CLASS = 'hidden-detail';
-var TINY_EFFECT_CLASS = 'is-tiny';
+var DETAIL_IMAGE_SELECTOR = "[data-image-role='target']";
+var DETAIL_TITLE_SELECTOR = "[data-image-role='title']";
+var DETAIL_FRAME_SELECTOR = "[data-image-role='frame']";
+var THUMBNAIL_LINK_SELECTOR = "[data-image-role='trigger']";
+var HIDDEN_DETAIL_CLASS = "hidden-detail";
+var TINY_EFFECT_CLASS = "is-tiny";
 var ESC_KEY = 27;
 
 function setDetails(imageUrl, titleText) {
-  'use strict';
+  "use strict";
 
   var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
-  detailImage.setAttribute('src', imageUrl);
+  detailImage.setAttribute("src", imageUrl);
 
   var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
   detailTitle.textContent = titleText;
 }
 
 function imageFromThumb(thumbnail) {
-  'use strict';
-  return thumbnail.getAttribute('data-image-url');
+  "use strict";
+  return thumbnail.getAttribute("data-image-url");
 }
 
 function titleFromThumb(thumbnail) {
-  'use strict';
-  return thumbnail.getAttribute('data-image-title')
+  "use strict";
+  return thumbnail.getAttribute("data-image-title");
 }
 
 function setDetailsFromThumb(thumbnail) {
-  'use strict';
+  "use strict";
   setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
 }
 
 function addThumbClickHandler(thumb) {
-  thumb.addEventListener('click', function(event) {
+  thumb.addEventListener("click", function(event) {
     event.preventDefault();
     setDetailsFromThumb(thumb);
     showDetails();
@@ -40,19 +40,19 @@ function addThumbClickHandler(thumb) {
 }
 
 function getThumbnailsArray() {
-  'use strict';
+  "use strict";
   var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
   var thumbnailArray = [].slice.call(thumbnails);
   return thumbnailArray;
 }
 
 function hideDetails() {
-  'use strict';
+  "use strict";
   document.body.classList.add(HIDDEN_DETAIL_CLASS);
 }
 
 function showDetails() {
-  'use strict';
+  "use strict";
   var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
   document.body.classList.remove(HIDDEN_DETAIL_CLASS);
   frame.classList.add(TINY_EFFECT_CLASS);
@@ -62,10 +62,10 @@ function showDetails() {
 }
 
 function addKeyPressHandler() {
-  'use strict';
-  document.body.addEventListener('keyup', function(event) {
+  "use strict";
+  document.body.addEventListener("keyup", function(event) {
     event.preventDefault();
-    console.log(event.keyCode);
+
     if (event.keyCode === ESC_KEY) {
       hideDetails();
     }
@@ -73,7 +73,7 @@ function addKeyPressHandler() {
 }
 
 function initializeEvents() {
-  'use strict';
+  "use strict";
   var thumbnails = getThumbnailsArray();
   thumbnails.forEach(addThumbClickHandler);
   addKeyPressHandler();
@@ -93,13 +93,13 @@ function mySlide(param) {
   var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
   var thumbnailArray = getThumbnailsArray();
   var currentIndex = getCurrentIndex(thumbnailArray, detailImage);
-  if (param === 'next') {
+  if (param === "next") {
     currentIndex = (currentIndex + 1) % thumbnailArray.length;
 
   } else {
     currentIndex = (currentIndex - 1 + thumbnailArray.length) % thumbnailArray.length;
   }
-  setDetails(thumbnailArray[currentIndex].href, thumbnailArray[currentIndex].getAttribute('data-image-title'))
+  setDetails(thumbnailArray[currentIndex].href, thumbnailArray[currentIndex].getAttribute("data-image-title"));
 }
 
 
